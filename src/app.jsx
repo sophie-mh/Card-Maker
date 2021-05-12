aa757ceb7827089568efa16734c8908acac655cc
Mihyun Sim <sophie.mihyun@gmail.com>
2021-05-12 22:01:01 +0900

    project set up & add firebase

---
+++ src/app.jsx
@@ -0,0 +1,19 @@
+import "./app.css";
+
+function App() {
+  return (
+    <body>
+      <div className="loginMenu">
+        <div className="header">Business Card Maker</div>
+        <div className="main">
+          <span className="login">Login</span>
+          <span className="google">Google</span>
+          <span className="email">Email</span>
+        </div>
+        <div className="footer">Code your dream</div>
+      </div>
+    </body>
+  );
+}
+
+export default App;
